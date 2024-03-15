44d83f8e091bd9a25ad67bd6da2eebe27de0ee5a
Max Katz <maxkatz6@outlook.com>
2024-03-14 18:36:19 -0700

    Add missing tests + enable headless

--- tests/Avalonia.Controls.ItemsRepeater.UnitTests/ItemsRepeaterTests.cs
+++ tests/Avalonia.Controls.ItemsRepeater.UnitTests/ItemsRepeaterTests.cs
@@ -1,24 +1,24 @@
 using System.Collections.ObjectModel;
+using Avalonia.Headless.XUnit;
 using Xunit;
 
-namespace Avalonia.Controls.UnitTests
-{
-    public class ItemsRepeaterTests
-    {
-        [Fact]
-        public void Can_Reassign_Items()
-        {
-            var target = new ItemsRepeater();
-            target.ItemsSource = new ObservableCollection<string>();
-            target.ItemsSource = new ObservableCollection<string>();
-        }
+namespace Avalonia.Controls.UnitTests;
 
-        [Fact]
-        public void Can_Reassign_Items_To_Null()
-        {
-            var target = new ItemsRepeater();
-            target.ItemsSource = new ObservableCollection<string>();
-            target.ItemsSource = null;
-        }
+public class ItemsRepeaterTests
+{
+    [AvaloniaFact]
+    public void Can_Reassign_Items()
+    {
+        var target = new ItemsRepeater();
+        target.ItemsSource = new ObservableCollection<string>();
+        target.ItemsSource = new ObservableCollection<string>();
+    }
+
+    [AvaloniaFact]
+    public void Can_Reassign_Items_To_Null()
+    {
+        var target = new ItemsRepeater();
+        target.ItemsSource = new ObservableCollection<string>();
+        target.ItemsSource = null;
     }
 }
---
+++ tests/Avalonia.Controls.ItemsRepeater.UnitTests/XamlStyleTests.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.ObjectModel;
+using Avalonia.Headless.XUnit;
+using Avalonia.Markup.Xaml;
+using Avalonia.Media;
+using Xunit;
+
+namespace Avalonia.Controls.UnitTests;
+
+public class XamlStyleTests
+{
+    [AvaloniaFact]
+    public void Style_Can_Use_NthChild_Selector_With_ItemsRepeater()
+    {
+        var xaml = @"
+<Window xmlns='https://github.com/avaloniaui'
+             xmlns:x='http://schemas.microsoft.com/winfx/2006/xaml'>
+    <Window.Styles>
+        <Style Selector='TextBlock'>
+            <Setter Property='Foreground' Value='Transparent'/>
+        </Style>
+        <Style Selector='TextBlock:nth-child(2n)'>
+            <Setter Property='Foreground' Value='{Binding}'/>
+        </Style>
+    </Window.Styles>
+    <ItemsRepeater x:Name='list' />
+</Window>";
+        var window = (Window)AvaloniaRuntimeXamlLoader.Load(xaml);
+        var collection = new ObservableCollection<IBrush>()
+        {
+            Brushes.Red, Brushes.Green, Brushes.Blue
+        };
+
+        var list = window.FindControl<ItemsRepeater>("list");
+        list.ItemsSource = collection;
+
+        window.Show();
+
+        IEnumerable<IBrush> GetColors() => Enumerable.Range(0, list.ItemsSourceView.Count)
+            .Select(t => (list.GetOrCreateElement(t) as TextBlock)!.Foreground);
+
+        Assert.Equal(new[] { Brushes.Transparent, Brushes.Green, Brushes.Transparent }, GetColors());
+
+        collection.Remove(Brushes.Green);
+
+        Assert.Equal(new[] { Brushes.Transparent, Brushes.Blue }, GetColors().ToList());
+
+        collection.Add(Brushes.Violet);
+        collection.Add(Brushes.Black);
+
+        Assert.Equal(new[] { Brushes.Transparent, Brushes.Blue, Brushes.Transparent, Brushes.Black }, GetColors());
+    }
+}
